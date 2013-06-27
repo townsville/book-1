7b982d700cecebf62b3bf2aebf156387544fd1f8
Brad Koehn <braddkoehn@gmail.com>
2013-06-26 20:12:30 -0500

    fix

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once ("etc/Michelf/Markdown.php");
-$my_text = file_get_contents('./Townsville.txt');
+require_once ("/etc/Michelf/Markdown.php");
+$my_text = file_get_contents('Townsville.txt');
 $my_html = Markdown::defaultTransform($my_text);
 ?>
