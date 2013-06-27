368b5aedd245f605f3a3d309d508c159df28feb8
Brad Koehn <braddkoehn@gmail.com>
2013-06-26 20:08:21 -0500

    php fix

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-use \etc\Michelf\Markdown;
+require_once ("etc/Michelf/Markdown.php");
 $my_text = file_get_contents('./Townsville.txt');
 $my_html = Markdown::defaultTransform($my_text);
 ?>
