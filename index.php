1e3d28708d6587af167b24ddc4aaba7e5196c786
Brad Koehn <braddkoehn@gmail.com>
2013-06-26 21:58:26 -0500

    Better styles

--- index.php
+++ index.php
@@ -2,5 +2,26 @@
 include_once "etc/markdown.php";
 $my_text = file_get_contents('townsville.md');
 $my_html = Markdown($my_text);
-echo $my_html;
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta id="viewport" name="viewport" content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+		<link href="etc/css/bootstrap.min.css" rel="stylesheet">
+		<link href="etc/css/bootstrap-responsive.min.css" rel="stylesheet">
+		<link href="etc/css/style.css" rel="stylesheet">
+	</head>
+
+<body>
+	<div class="container-narrow">
+		<div class="row-fluid">
+			<div class="span12">
+	<?php
+				echo $my_html;
+	?>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
