717e8c7658850f93d7032895331ecde1f27bda9c
Brad Koehn <braddkoehn@gmail.com>
2013-06-26 20:29:24 -0500

    fix

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include_once "etc/markdown.php";
-$my_text = file_get_contents('Townsville.txt');
+$my_text = file_get_contents('townsville.md');
 $my_html = Markdown($my_text);
 echo $my_html;
 ?>
